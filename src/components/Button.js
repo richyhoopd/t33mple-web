370d2cf9ee2577b4d843d224dc14bf0e1ef92398
Ricardo Martínez <theilluminatiduck@gmail.com>
2025-05-31 01:15:00 -0500

    cambio en favicon e imagen de link

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,10 @@ import React from 'react';
 import '../styles/components/button.css'
 
 export default function Button() {
+
   return (
     <div className='fucking-button'>
-        <button>VAMPONIGHT</button>
+        <button onClick={() => alert("Aun no, espera un poco mas :)")}>VAMPONIGHT</button>
     </div>
   )
 }
