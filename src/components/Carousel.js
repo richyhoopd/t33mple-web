85c9747c61c6d436903a277ac633ca5506261644
Ricardo Martínez <theilluminatiduck@gmail.com>
2025-05-31 01:05:49 -0500

    responsive done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,7 +6,7 @@ export default function Carousel() {
   return (
     <div className='carousel'>
         <h1>You know what you are doing here. Do not fool yourself.</h1>
-        <img src={car} alt="a car" />
+        <img src={car} alt="a car" id="car-image"/>
     </div>
   )
 }
